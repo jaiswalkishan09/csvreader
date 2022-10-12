840627f38dba46e66f8757aae6dcb61c8ec0f6fa
Kishan Jaiswal <jaiswalkishan09@gmail.com>
2022-10-12 09:17:23 +0545

    read csv logic completed

--- src/common/commonFunctions.js
+++ src/common/commonFunctions.js
@@ -24,4 +24,5 @@ async function removeFile(filePath)
 }
 
 
+
 module.exports={removeFile}
--- src/controllers/csvController.js
+++ src/controllers/csvController.js
@@ -23,37 +23,63 @@ const readCsv=async (req,res)=>{
                     results.push(row);
                 })
                 .on("end", function () {
-                    if(results.length>0 && Object.keys(results[0]).length==1)
+                    let keys=Object.keys(results[0])[0];
+                    let keysValue=keys;
+                    keys=keys.split(';');
+                    if(results.length>0 && keys.length>=1)
                     {
-                      let keys=Object.keys(results[0])[0];
-                      let keysValue=keys;
-                      keys=keys.split(';');
-                      if(keys.length>0)
-                      {
-                          let ans=[];
-                          for(let i=0;i<results.length;i++)
-                          {
-                              let value=results[i][keysValue];
-                              let values=value.split(';');
-                              let keyValue={};
-                              for(let j=0;j<keys.length;j++)
-                              {
-                                  if(j<values.length)
-                                  {
-                                     keyValue[keys[j]]=values[j];
-                                  }
-                              }
-                              ans.push(keyValue);
-                          }
-                          removeFile(fileName);
-                          return res.status(200).json({result:ans});
-                      }
+                        let ans=[];
+                        for(let i=0;i<results.length;i++)
+                        {
+                            let value=results[i][keysValue];
+                            let values=value.split(';');
+                            let allKeys=Object.keys(results[i]);
+                            let keyValue={};
+                            let j=0;
+                            while(j<values.length && j<keys.length)
+                            {  
+                                keyValue[keys[j]]=values[j];
+                                j++;
+                            }
+                            for(let k=1;k<allKeys.length;k++)
+                            {
+                                let allKeyValue=results[i][allKeys[k]].split(';');
+
+                                if(allKeyValue.length>1)
+                                {   j--;
+                                    if(j<keys.length)
+                                    {
+                                        keyValue[keys[j]]=keyValue[keys[j]]+','+allKeyValue[0];
+                                        j++;
+                                    }
+                                    for(let l=1;l<allKeyValue.length;l++)
+                                    {
+                                        if(j<keys.length)
+                                        {
+                                            keyValue[keys[j]]=allKeyValue[l];
+                                        }
+                                        j++;
+                                    }
+                                }
+                                else{
+                                    if(j<=keys.length)
+                                    {
+                                        j--;
+                                        keyValue[keys[j]]=keyValue[keys[j]]+','+allKeyValue[0];
+                                        j++;
+                                    }
+                                }
+                            }
+                            ans.push(keyValue);
+                        }
+                        // removeFile(fileName);
+                        return res.status(200).json({result:ans});
                     }
-                    removeFile(fileName);
-                    return res.status(200).json({result:result});
+                    // removeFile(fileName);
+                    return res.status(200).json({result:results});
                 })
                 .on("error", function (error) {
-                    removeFile(fileName);
+                    // removeFile(fileName);
                     console.log("Error in getListOfJsonFromCsv main catch block",error);
                     throw("Error while getting json from csv");
                 })
